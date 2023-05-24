452eaa5cb2792d96221cc3531bb872e21418b015
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-24 14:31:10 +0900

    refactor : SimpleConnectionMaker 적용

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -7,11 +7,13 @@ import java.util.Map;
 
 import static java.lang.System.getenv;
 
-public abstract class UserDao {
+public class UserDao {
+
+    SimpleConnectionMaker connectionMaker = new SimpleConnectionMaker();
 
     public void add(User user) throws ClassNotFoundException, SQLException {
 
-        Connection conn = getConnection();
+        Connection conn = connectionMaker.makeNewConnection();
 
         PreparedStatement psmt = conn.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
         psmt.setString(1, user.getId());
@@ -25,7 +27,7 @@ public abstract class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection conn = getConnection();
+        Connection conn = connectionMaker.makeNewConnection();
 
         PreparedStatement pstmt = conn.prepareStatement("select id, name, password from users where id = ?");
         pstmt.setString(1, id);
@@ -45,8 +47,8 @@ public abstract class UserDao {
         return user;
     }
 
-    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
-    /*{
+    /*public Connection getConnection() throws ClassNotFoundException, SQLException;
+    {
         Map<String, String> env = getenv();
         String dbHost = env.get("DB_HOST");
         String dbUser = env.get("DB_USER");
@@ -61,14 +63,14 @@ public abstract class UserDao {
 
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new NUserDao();
+        UserDao userDao = new UserDao();
         User user = new User();
-        user.setId("4");
-        user.setName("park");
+        user.setId("5");
+        user.setName("king");
         user.setPassword("1234");
-//        userDao.add(user);
+        userDao.add(user);
 
-        User selectedUser = userDao.get("4");
+        User selectedUser = userDao.get("5");
         System.out.println(selectedUser.getId());
         System.out.println(selectedUser.getName());
         System.out.println(selectedUser.getPassword());
