90ff4765c8efb4075f9067201526396a48dafde2
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-24 15:07:24 +0900

    refactor : interface 적용

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -9,11 +9,15 @@ import static java.lang.System.getenv;
 
 public class UserDao {
 
-    SimpleConnectionMaker connectionMaker = new SimpleConnectionMaker();
+    ConnectionMaker connectionMaker;
+    public UserDao() {
+        this.connectionMaker = new DConnectionMaker();
+    }
+
 
     public void add(User user) throws ClassNotFoundException, SQLException {
 
-        Connection conn = connectionMaker.makeNewConnection();
+        Connection conn = connectionMaker.makeConnection();
 
         PreparedStatement psmt = conn.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
         psmt.setString(1, user.getId());
@@ -27,7 +31,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection conn = connectionMaker.makeNewConnection();
+        Connection conn = connectionMaker.makeConnection();
 
         PreparedStatement pstmt = conn.prepareStatement("select id, name, password from users where id = ?");
         pstmt.setString(1, id);
@@ -65,12 +69,12 @@ public class UserDao {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
         User user = new User();
-        user.setId("5");
-        user.setName("king");
+        user.setId("6");
+        user.setName("king22");
         user.setPassword("1234");
         userDao.add(user);
 
-        User selectedUser = userDao.get("5");
+        User selectedUser = userDao.get("6");
         System.out.println(selectedUser.getId());
         System.out.println(selectedUser.getName());
         System.out.println(selectedUser.getPassword());
